9bb34d1a9b973155db095e57e4bfc9f0c286eb90
Dimitry-P <dimitry.piatnitsky@yahoo.com>
2024-12-23 16:56:10 +0300

    Add project files.

---
+++ Task_14/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14
+{
+    internal class Program
+    {
+        //•	Задача 14: Напишите программу, которая
+        //находит сумму всех чисел в диапазоне от M до N.
+        public static int result = 0;
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите начальное и конечное число через запятую");
+            string num = Console.ReadLine();
+            string[] pieces = num.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            int m = int.Parse(pieces[0]);
+            int n = int.Parse(pieces[1]);
+            for(int i = m; i < n+1; i++)
+            {
+                result += i;
+            }
+            Console.WriteLine("Сумма чисел равна: "+result);
+        }
+    }
+}
